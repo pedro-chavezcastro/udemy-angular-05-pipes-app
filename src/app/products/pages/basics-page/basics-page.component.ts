65f42a2c71c11535ed23601a38d116b2ed98a297
pedro-chavezcastro <pedro.chavezcastro@outlook.com>
2024-10-22 16:16:00 -0500

    First commit

---
+++ src/app/products/pages/basics-page/basics-page.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-basics-page',
+  templateUrl: './basics-page.component.html',
+  styleUrl: './basics-page.component.css'
+})
+export class BasicsPageComponent {
+
+  public nameLower: string = 'pedro';
+  public nameUpper: string = 'PEDRO';
+  public fullName: string = 'PeDrO CHaVEz';
+
+  public customDate: Date = new Date();
+
+}
